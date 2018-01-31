b466349bcd3f4d0c812b17e5d8b201599dd92592
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-01-31 22:48:11 +0530

    adds routing to posts/new component

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return (
+            <div>
+                Make a fresh new Post right here bro.
+            </div>
+        )
+    }
+}
+
+export default PostsNew;
