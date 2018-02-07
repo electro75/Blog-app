c2f19c3040d5f5ce5fa67be6317b41f4d2d72c97
Sahil Naikdhure <sanaikdhure@gmail.com>
2018-02-07 22:18:34 +0530

    shows individual posts, minor bug fixes

--- src/reducers/posts.reducer.js
+++ src/reducers/posts.reducer.js
@@ -1,14 +1,20 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 
 export default function (state= { }, action) {
-    // console.log(action);
+    console.log(action);
     switch(action.type) {
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, 'id');
 
-        
+        case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = { ...state }; 
+            // newState[post.id] = post;
+            // return newState;
+            //the below line is equivalent to the above commented lines.
+            return { ...state, [action.payload.data.id] : action.payload.data }
 
         default :
             return state;
